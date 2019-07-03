96cc376803004d0bca09942b9bdfbb6d8c99a8d2
Andrew <andsmi97@yandex.ru>
2019-07-03 13:03:54 +0300

    added menu items styles

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./styles.scss";
 
-export default ({ title }) => {
+export default ({ title, img, size }) => {
   return (
-    <div className="menu-item">
+    <div className={`${size ? size : ""} menu-item`}>
+      <div
+        style={{
+          backgroundImage: `url(${img})`
+        }}
+        className="background-image"
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Directory from "../../components/Directory";
+import "./styles.scss";
+export default () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
